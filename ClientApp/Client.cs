643d12f9442c98ce463d8a968a36f3d5e01f4578
Pratik Kumar <pkumar@tavisca.com>
2017-07-21 21:20:36 +0530

    new server

---
+++ ClientApp/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPAddress ipAddress = IPAddress.Parse("192.168.6.53");
+
+
+
+            Console.WriteLine("Enter your name :");
+            string name = Console.ReadLine();
+
+            TcpClient tcpclnt = new TcpClient();
+            //Console.WriteLine(((IPEndPoint)tcpclnt.Client.LocalEndPoint).Address.ToString());
+            tcpclnt.Connect("192.168.6.16", 500);
+
+            while (true)
+            {
+                //String str = Console.ReadLine();
+                string message = Console.ReadLine(); // string.Format("{0}:\t {1}", name, str);
+                
+
+                Stream stm = tcpclnt.GetStream();
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(message);
+                stm.Write(ba, 0, ba.Length);
+                byte[] data = new byte[1024];
+                //  Console.WriteLine(((IPEndPoint)tcpclnt.Client.LocalEndPoint).Address.ToString());
+                NetworkStream stream = tcpclnt.GetStream();
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    do
+                    {
+                        stream.Read(data, 0, data.Length);
+                        memoryStream.Write(data, 0, data.Length);
+                    } while (stream.DataAvailable);
+
+                    string str1 = System.Text.Encoding.ASCII.GetString(memoryStream.ToArray(), 0, (int)memoryStream.Length).Trim('\0');
+                    Console.WriteLine(str1);
+                }
+                // ResponseAsync(ipAddress, Convert.ToInt32((((IPEndPoint)tcpclnt.Client.LocalEndPoint).Port.ToString())));
+                //tcpclnt.Close();
+            }
+
+
+        }
+
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,58 +4,229 @@ using System.Net.Sockets;
 using System.Text;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
+    public class Client
+    {
+        public Client(string ipAddress, int port)
+        {
+            this.IPAddress = ipAddress;
+            this.Port = port;
+        }
+
+        public string IPAddress { get; set; }
+
+        public int Port { get; set; }
+
+        public string Alias { get; set; }
+    }
+
     class Program
     {
-        public static HashSet<string> IP_Addresses = new HashSet<string>();
+        static string myIP = "192.168.6.16";
+        static IPAddress localAddress = IPAddress.Parse(myIP);
+        static TcpListener server = new TcpListener(localAddress, 500);
+
+        public static List<Client> lstClient = new List<Client>();
+        public static List<TcpClient> setOfTcpClients = new List<TcpClient>();
+        public static List<KeyValuePair<string, string>> _ipToNameMapping = new List<KeyValuePair<string, string>>();
+
         static void Main(string[] args)
         {
-            string myIP = "192.168.6.16";
-
-            IPAddress localAddress = IPAddress.Parse(myIP);
-            TcpListener server = new TcpListener(localAddress, 500);
-
             server.Start();
 
             while (true)
             {
-                Socket clientSocket = server.AcceptSocket();
-                byte[] incomingStream = new byte[2048];
-                clientSocket.Receive(incomingStream);
+                //TODO: determine multiple clients or single client
+                TcpClient newClient = server.AcceptTcpClient();
+                Console.WriteLine("{0} connected..", (newClient.Client.RemoteEndPoint as IPEndPoint).Address);
+                setOfTcpClients.Add(newClient);
 
-                string message = Encoding.ASCII.GetString(incomingStream).Trim('\0');
-
-                string clientIP = (clientSocket.RemoteEndPoint as IPEndPoint).Address.ToString();
-                IP_Addresses.Add(clientIP);
-                Console.WriteLine("IP: {0} | {1}", clientIP, message);
-
-                //logging
-                using (StreamWriter sw = new StreamWriter("chatLogs.txt", true))
+                Task.Run(() =>
                 {
-                    sw.WriteLine("IP: {0} | {1}", clientIP, message);
-                }
-
-                //sending to all other clients
-                foreach (string ip in IP_Addresses)
-                {
-                    if (!ip.Equals(clientIP))
+                    while (true)
                     {
-                        try
+                        ReadMessage();
+                    }
+                });
+            }
+
+            //chatting loop
+            //while (true)
+            //{
+
+
+            ////var client = HandleClient(clientSocket);
+
+            //LogMessage(client, message);
+
+
+
+            ////logging
+
+
+            ////sending to all other clients
+            //Console.Write("Sending To: ");
+            //foreach (var client in lstClient)
+            //{
+            //    if (!client.IPAddress.Equals(clientIP))
+            //    {
+            //        try
+            //        {
+            //            TcpClient tcpClient = new TcpClient(client.IPAddress, client.Port);
+            //            NetworkStream tcpStream = tcpClient.GetStream();
+            //            tcpStream.Write(Encoding.ASCII.GetBytes(message), 0, message.Length);
+            //            Console.Write("{0}:{1},", client.IPAddress, client.Port);
+            //            tcpClient.Close();
+            //        }
+            //        catch (Exception e)
+            //        {
+            //            Console.WriteLine(e.Message);
+            //            continue;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //clientSocket.Close();
+            //clientSocket.Dispose();
+            //}
+        }
+        private static bool ContainsKey(string key)
+        {
+            return _ipToNameMapping.Any(kvp => string.Equals(kvp.Key, key, StringComparison.InvariantCultureIgnoreCase));
+        }
+        private static void ReadMessage()
+        {
+            for (int i = 0; i < setOfTcpClients.Count; i++)
+            {
+
+                try
+                {
+                    var item = setOfTcpClients[i];
+                    if (item == null)
+                        continue;
+                    var ip = (item.Client.RemoteEndPoint as IPEndPoint).Address.ToString();
+
+                    if (!item.Connected) continue;
+                    NetworkStream clientStream = item.GetStream();
+                    if (!clientStream.DataAvailable)
+                    {
+                        continue;
+                    }
+                    byte[] clientByteStream = new byte[2048];
+                    clientStream.Read(clientByteStream, 0, clientByteStream.Length);
+                    string message = Encoding.ASCII.GetString(clientByteStream).Trim('\0');
+                    //Console.WriteLine(message);
+                    if (ContainsKey(ip) == false)
+                        _ipToNameMapping.Add(new KeyValuePair<string, string>(ip, ip));
+
+                    if (message.ToLowerInvariant().StartsWith("name:"))
+                    {
+                        var name = message.Split(':')[1];
+                        _ipToNameMapping.RemoveAll(k => string.Equals(k.Key, ip));
+                        _ipToNameMapping.Add(new KeyValuePair<string, string>(ip, name));
+                    }
+                    else if (message.ToLowerInvariant().StartsWith("get:"))
+                    {
+                        var clients = string.Empty;
+                        foreach (var key in _ipToNameMapping)
                         {
-                            TcpClient tcpClient = new TcpClient(ip, 500);
-                            NetworkStream tcpStream = tcpClient.GetStream();
-                            tcpStream.Write(Encoding.ASCII.GetBytes(message), 0, message.Length);
-                            tcpClient.Close();
+                            clients += key.Value + " ";
                         }
-                        catch { }
+
+                        Broadcast(clients, string.Empty, ip);
+                    }
+                    else if(message.ToLowerInvariant().StartsWith("to:"))
+                    {
+                        var to = message.Split(' ')[0].Split(':')[1];
+                        message = message.Replace("to:" + to, "");
+                        var kvp = _ipToNameMapping.FirstOrDefault(k => string.Equals(k.Value, to, StringComparison.InvariantCultureIgnoreCase));
+                        if (string.IsNullOrWhiteSpace(kvp.Key) == false)
+                        {
+                            Broadcast(message, kvp.Value, kvp.Key);
+                        }
+                        
+                    }
+                    else
+                    {
+                        var kvp = _ipToNameMapping.FirstOrDefault(k => string.Equals(k.Key, ip));
+                        Broadcast(message, kvp.Value,  null);
                     }
                 }
+                catch (Exception)
+                {
 
-                clientSocket.Close();
-                clientSocket.Dispose();
+                }
+
             }
         }
+
+        private static void Broadcast(string message, string name, string ip)
+        {
+            if (string.IsNullOrWhiteSpace(name) == false)
+                message = string.Format("[{0}]:{1}\n", name, message);
+            
+            Console.WriteLine(message);
+            var x = new List<TcpClient>(setOfTcpClients);
+
+            if (string.IsNullOrWhiteSpace(ip) == false)
+            {
+                var client = setOfTcpClients.FirstOrDefault(c => string.Equals((c.Client.RemoteEndPoint as IPEndPoint).Address.ToString(), ip));
+                if(client != null && client.Connected)
+                {
+                    Process(client, message);
+                    return;
+                }
+            }
+            
+            //Console.WriteLine(message);
+            foreach (var client in x)
+            {
+                Process(client, message);
+            }
+        }
+
+        private static void Process(TcpClient client, string message)
+        {
+            if (!client.Connected) return;
+            NetworkStream clientStream = client.GetStream();
+            try
+            {
+                clientStream.Write(Encoding.ASCII.GetBytes(message), 0, message.Length);
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message);
+            }
+        }
+
+        private static Client HandleClient(Socket socket)
+        {
+            string clientIP = (socket.RemoteEndPoint as IPEndPoint).Address.ToString();
+            int clientPort;
+            int.TryParse((socket.RemoteEndPoint as IPEndPoint).Port.ToString(), out clientPort);
+
+            if (lstClient.FirstOrDefault(client => string.Equals(client.IPAddress, clientIP)) == null)
+            {
+                lstClient.Add(new Client(clientIP, clientPort));
+            }
+
+            return new Client(clientIP, clientPort);
+        }
+
+        private static void LogMessage(Client client, string message)
+        {
+            Console.WriteLine("IP: {0}:{1} | {2}", client.IPAddress, client.Port, message);
+
+            using (StreamWriter sw = new StreamWriter("chatLogs.txt", true))
+            {
+                sw.WriteLine("IP: {0}:{1} | {2}", client.IPAddress, client.Port, message);
+            }
+        }
+
     }
 }
