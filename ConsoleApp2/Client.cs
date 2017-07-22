738fe8e7c1f031de9537e6ecd60fa724fb995220
Pratik Kumar <pkumar@tavisca.com>
2017-07-22 16:39:22 +0530

    server made with commands

---
+++ ConsoleApp2/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApp2
+{
+    class Client
+    {
+        public string IpAddess { get; set; }
+        public string Name { get; set; }
+
+        public Client(string IpAddess)
+        {
+            this.IpAddess = IpAddess;
+        }
+
+        override
+        public string ToString()
+        {
+            if (Name == null) return IpAddess;
+            else return Name;
+        }
+    }
+}
